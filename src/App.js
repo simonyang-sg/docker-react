fd2044182712d22769ad62f2b1424c4b4e796635
Simon Yang <simonyang.sg@gmail.com>
2021-01-15 00:36:00 +0800

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          I am a feature branch change!
         </p>
         <a
           className="App-link"
